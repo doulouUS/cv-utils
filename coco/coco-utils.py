05f3b1fcc44e8ccbf4cc885f012b931d394e8867
louis-l-douge <louis.l.douge@axa.com>
2021-08-31 18:34:22 +0200

    Adding function to transform manifest file to COCO JSON while squashing multiple instances into one in the output

--- coco/coco-utils.py
+++ coco/coco-utils.py
@@ -89,6 +89,7 @@ def generate_coco_json_from_masks(
     
     pass
 
+
 def generate_coco_json_from_tfRecords(tfrecords_path: str):
     """[summary]
 
@@ -119,7 +120,6 @@ def generate_coco_json_from_tfRecords(tfrecords_path: str):
         images.append(image)
     
         # Binary Mask 
-        # TODO: doesn't work
         tfr_mask = example.features.feature["image/object/mask"].bytes_list.value[0]
         m = Image.open(BytesIO(tfr_mask))
         numpy_binary_mask = np.asarray(m)
@@ -150,6 +150,126 @@ def generate_coco_json_from_tfRecords(tfrecords_path: str):
             }
         ]
     }
+
+def generate_coco_json_from_manifest(input_manifest, write=False):
+    """Transform a manifest file to COCO Json format.
+    The manifest file (obtained from SageMaker groundtruth) contains annotations with 
+    a single class (TODO here `categories` is hardcoded, to be parameterized).
+    However, there can be several instances per class. They are all squashed
+    together. The output COCO JSON file is therefore made for semantic segmentation
+    with one annotation per image.
+
+    Args:
+        input_manifest (str): path to manifest file
+        write (bool, optional): whether to write the dict to json. Defaults to False.
+
+    Returns:
+        dict: coco json as a python dict
+    """
+    date_now = datetime.today().strftime("%Y-%m-%d")
+    year = datetime.today().strftime("%Y")
+
+    # TODO parameterize
+    licenses = [{"name": "", "id": 0, "url": ""}]
+    info = {
+        "contributor": "AXA GETD - AXA Morocco",
+        "date_created": date_now,
+        "description": "Car Damage Dataset",
+        "url": "",
+        "version": 0.1,
+        "year": "2021",
+    }
+    # TODO parameterize
+    category = {
+        "color":"#b00cd4",
+        "id":1,
+        "keypoint_colors":[],
+        "metadata":{},
+        "name":"Dégâts",
+        "supercategory":""
+    }
+
+    with open(input_manifest, "r") as f:
+        lines = f.readlines()
+
+    images = []
+    for i, sample in enumerate(lines):
+        sample = json.loads(sample)
+        annot_data = sample.get('damage').get('annotationsFromAllWorkers')[0].get('annotationData')
+        content  = json.loads(annot_data.get('content'))
+
+        sample['height'] = content.get('annotatedResult').get('inputImageProperties').get('height')
+        sample['width'] = content.get('annotatedResult').get('inputImageProperties').get('width') 
+        try:
+            image = {
+                "coco_url": "",
+                "date_captured": "",
+                "flickr_url": "",
+                "license": 0,
+                "id": i,
+                "file_name": Path(sample['source-ref']).name,
+                "height": content.get('annotatedResult').get('inputImageProperties').get('height'),
+                "width": content.get('annotatedResult').get('inputImageProperties').get('width'),
+                "angle_object": content['angle_object'],
+                 "brand": content['brand'],
+                 "car_type": content['car_type'],
+                 "hubcaps": content['hubcaps'],
+                 "rim_tire": content['rim_tire']
+            }
+        except:
+            # TODO this is a very specific need from how we handled our data
+            # it should probably be abstracted to make the code more general to
+            # any cases
+            # TODO license and other fields are hardcoded 
+            # For curated images, additional data is not in `content` but in the first level of the json
+            image = {
+                "coco_url": "",
+                "date_captured": "",
+                "flickr_url": "",
+                "license": 0,
+                "id": i,
+                "file_name": Path(sample['source-ref']).name,
+                "height": content.get('annotatedResult').get('inputImageProperties').get('height'),
+                "width": content.get('annotatedResult').get('inputImageProperties').get('width'),
+                "angle_object": sample['angle_object'],
+                 "brand": sample['brand'],
+                 "car_type": sample['car_type'],
+                 "hubcaps": sample['hubcaps'],
+                 "rim_tire": sample['rim_tire']
+            }
+        images.append(image)
+
+    annotations = []
+    for img_id, sample in enumerate(lines):
+        sample = json.loads(sample)
+        annot_data = sample.get('damage').get('annotationsFromAllWorkers')[0].get('annotationData')
+        content  = json.loads(annot_data.get('content'))
+
+        mask_bytes = content.get('annotatedResult').get('labeledImage').get('pngImageData')
+        mask = Image.open(io.BytesIO(base64.b64decode(mask_bytes)))
+        # Convert to grayscale to squash all different instances into one
+        mask_image = mask.convert('L')
+        binary_mask = np.asarray(mask_image) > 0
+
+        # One annotation per image, so image_id and annot_id are the same
+        annot = binary_mask2coco_annot(
+            binary_mask,
+            img_id,
+            1,
+            img_id
+        )
+        annotations.append(annot)
+    coco_json = {
+        "info": info,
+        "licenses": licenses,
+        "images": images,
+        "annotations": annotations,
+        "categories": category
+    }
+    if write:
+        with open("coco-json-from-manifest.json", "w") as f:
+            json.dumps(coco_json, f)
+    return coco_json
 
 def img2mask(coco, img_id, cat_id):
     anns_ids = coco.getAnnIds(imgIds=[img_id], catIds=[cat_id], iscrowd=None)
@@ -201,6 +321,7 @@ def compute_IoU_from_coco_json(coco_gt, coco_dt, coco_type='segm'):
     return mean_iou
 
 
+
 if __name__ == "__main__":
 
     tfrecords = Path("./sample_data/tfrecords")
@@ -210,12 +331,12 @@ if __name__ == "__main__":
     coco = Path("./sample_data/coco_json")
     test_coco = coco / "val_car_damages.json"
 
-    d = generate_coco_json_from_tfRecords(str(train_tfr))
-    with open(coco / "train_car_damages_2.json", "w") as f:
+    d = generate_coco_json_from_tfRecords(str(test_tfr))
+    with open(coco / "val_car_damages_2.json", "w") as f:
         json.dump(d, f)
 
     mean_iou = compute_IoU_from_coco_json(
-        str(coco / "train_car_damages.json"),
-        str(coco / "train_car_damages.json")
+        str(coco / "val_car_damages.json"),
+        str(coco / "val_car_damages_2.json")
     )
-    
+    print("")
